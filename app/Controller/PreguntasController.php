784ae85dddd8b91517c7df222e8713c6cf55edc2
Andrea <andreasanchezblanco@gmail.com>
2015-11-20 16:33:54 +0100

    CakePHP

---
+++ app/Controller/PreguntasController.php
@@ -0,0 +1,16 @@
+<?php
+class PreguntasController extends AppController {
+    public $helpers = array('Html', 'Form');
+    
+    /*funcion index que se corresponde con la vista index.ctp
+    La vista index.ctp podra acceder a la variable preguntas, que contiene todas las filas de la tabla
+    Pregunta*/
+    public function index() {
+        $this->set('preguntas', $this->Pregunta->find('all'));
+        $this->layout = 'faq_life';
+    }
+    public function pregunta() {
+        
+    }
+}
+?>
